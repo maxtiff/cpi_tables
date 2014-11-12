3c6458b53d976eecc8a1bd5f7703c34c64cfa5c9
Travis <travismay@gmail.com>
2014-11-12 14:38:31 -0600

    Filter tables by city

--- filter.R
+++ filter.R
@@ -3,6 +3,15 @@ filter <- function(data, frequency, adjustment) {
 
   data <- data[data[,4] == adjustment & data[,5] == frequency & data[,9] <= 3,]
 
-  return(data[order(data$sort_sequence.x),])
+  data <- data[order(data$sort_sequence.x),]
+
+  return(data[order(data$sort_sequence.y),])
+
+}
+
+regionFilter <- function(area) {
+
+# for(item in area$area_name) { print(as.character(item))}
+  data <- data[grepl("(base)$",data$item_name),]
 
 }
